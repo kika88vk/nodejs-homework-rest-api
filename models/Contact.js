630c5e2745118cd1e73b7a35de03936709c9cd77
kika88vk <69463446+kika88vk@users.noreply.github.com>
2023-10-08 19:05:54 +0300

    add get, post, put, patch & delete

--- models/Contact.js
+++ models/Contact.js
@@ -1,4 +1,6 @@
 import { Schema, model } from "mongoose";
+import { handleSaveError, runValidatorsAtUpdate } from "./hooks.js";
+import Joi from 'joi';
 
 const contactSchema = new Schema({
     name: {
@@ -6,7 +8,7 @@ const contactSchema = new Schema({
         required: [true, 'Set name for contact']
     },
     email: {
-        tye: String,
+        type: String,
     },
     phone: {
         type: String,
@@ -15,6 +17,35 @@ const contactSchema = new Schema({
         type: Boolean,
         default: false,
     },
+}, { versionKey: false, timestamps: true })
+
+contactSchema.post("save", handleSaveError);
+
+contactSchema.pre("findOneAndUpdate", runValidatorsAtUpdate);
+
+contactSchema.post("findOneAndUpdate", handleSaveError);
+
+export const contactAddSchema = Joi.object({
+    name: Joi.string().required().messages({
+        "any.required": "missing required name field",
+    }),
+    email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }).required().messages({
+        "any.required": "missing required email field",
+    }),
+    phone: Joi.string().required().messages({
+        "any.required": "missing required phone field",
+    }),
+    favorite: Joi.boolean()
+})
+
+export const contactUpdateSchema = Joi.object({
+    name: Joi.string(),
+    email: Joi.string().email(),
+    phone: Joi.string(),
+}).or("name", "email", "phone");
+
+export const contactUpdateFavoriteSchema = Joi.object({
+    favorite: Joi.boolean().required()
 })
 
 const Contact = model("contact", contactSchema);
